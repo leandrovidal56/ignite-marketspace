75bdd2a71e2994dd69874c41a9a1462832b176b8
LeandroVidal56 <leandrovidal56@gmail.com>
2023-06-26 20:34:06 -0300

    start integrate app to api

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,23 +1,40 @@
-import { Input as NativeBaseInput, IInputProps } from 'native-base';
+import { Input as NativeBaseInput, IInputProps, FormControl  } from 'native-base';
 
-export function Input({ ...rest }: IInputProps) {
+type Props =  IInputProps & {
+    errorMessage?: string | null;
+}
+
+export function Input({errorMessage = null, isInvalid, ...rest}: Props){
+        const invalid = !!errorMessage || isInvalid;
+
     return (
-        <NativeBaseInput
-            bg='#F7F7F8'
-            h={12}
-            px={4}
-            borderWidth={0}
-            fontSize="md"
-            color="black"
-            fontFamily="body"
-            mt={4}
-            placeholderTextColor="#9F9BA1"
-            _focus={{
-                bg: '#F7F7F8',
-                borderWidth: 1,
-                borderColor: '#647AC7'
-            }}
-            {...rest}
-        />
+        <FormControl isInvalid={invalid}>
+            <NativeBaseInput
+                bg='#F7F7F8'
+                h={12}
+                px={4}
+                borderWidth={0}
+                fontSize="md"
+                color="black"
+                fontFamily="body"
+                mt={4}
+                placeholderTextColor="#9F9BA1"
+                isInvalid={invalid}
+                _invalid={{
+                    borderWidth: 1,
+                    borderColor: "red.500"
+                }}
+
+                _focus={{
+                    bg: '#F7F7F8',
+                    borderWidth: 1,
+                    borderColor: '#647AC7'
+                }}
+                {...rest}
+                />
+                <FormControl.ErrorMessage _text={{color: "red.500" }} >
+                    {errorMessage}
+                </FormControl.ErrorMessage>
+        </FormControl>
     )
 }
